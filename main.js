9eec9030fa3c5a52ad450f9df6d8d49e2a489c81
varles0 <41309786+varles0@users.noreply.github.com>
2020-09-10 17:58:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -18,61 +18,140 @@
 
     const desktopNav = document.querySelector(".desktop-nav");
     const navbarTop = desktopNav.offsetTop;
+    const titularNavbar = document.querySelector(".nav-empresa");
+
+    const formContacto = document.querySelector("#form-contacto");
+    const mensajeFormulario = document.querySelector(".mensaje-contacto");
+
+    // const pantallaCarga = document.querySelector('.pantalla-carga');
+
+    // setTimeout(()=>{
+
+    //   // console.log('carga completa?');
+    //   // pantallaCarga.classList.add('carga-completa')
+    //   setTimeout(()=>{
+    //     console.log('borrando pantalla del dom');
+    //     pantallaCarga.remove();
+    //   },1000);
+    //   // pantallaCarga.remove();
+
+    // },2000)
+
+    formContacto.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const formConstraints = {
+        nombre: {
+          presence: { allowEmpty: false, message: " no debe ir vacío" },
+          length: {
+            minimum: 2,
+            tooShort: " debe tener al menos 2 caracteres",
+            maximum: 30,
+            tooLong: " debe tener como máximo 30 caracteres",
+          },
+        },
+        email: {
+          presence: {
+            allowEmpty: false,
+            message: " no debe ir vacío",
+          },
+          email: { message: " debe ser válido" },
+        },
+        mensaje: {
+          presence: {
+            allowEmpty: false,
+            message: " no debe ir vácio",
+          },
+        },
+      };
+
+      const formValues = {
+        nombre: formContacto.elements.nombre.value,
+        email: formContacto.elements.email.value,
+        mensaje: formContacto.elements.mensaje.value,
+      };
+
+      // var formBody = new FormData();
+      // formBody.set("nombre", formContacto.elements.nombre.value);
+      // formBody.set("email", formContacto.elements.email.value);
+      // formBody.set("mensaje", formContacto.elements.mensaje.value);
+
+      let errors = validate(formValues, formConstraints);
+      //console.log(errors.nombre);
+
+      if (!errors) {
+        fetch("/form-contacto.php", {
+          method: "POST",
+          mode: "same-origin",
+          credentials: "same-origin",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formValues),
+        })
+          .then((res) => {
+            if (res.ok) {
+              return res.json();
+            } else {
+              throw "Error enviando la información";
+            }
+          })
+          .then(function (res) {
+            formContacto.elements.nombre.value = "";
+            formContacto.elements.email.value = "";
+            formContacto.elements.mensaje.value = "";
 
 
-    const iconoBajar = document.querySelector('.icono-bajar');
-    const pantallaCarga = document.querySelector('.pantalla-carga');
+            mensajeFormulario.style.opacity = 1;
+            mensajeFormulario.style.color = "black";
+            mensajeFormulario.innerHTML = "Mensaje enviado exitosamente";
 
-    setTimeout(()=>{
+            setTimeout(() => {
+              mensajeFormulario.style.opacity = 0;
+              mensajeFormulario.innerHTML = "";
+            }, 5000);
+          })
+          .catch(function (err) {
+            mensajeFormulario.style.opacity = 1;
+            mensajeFormulario.style.color = "red";
+            mensajeFormulario.innerHTML = err;
 
-      console.log('carga completa?');
-      pantallaCarga.classList.add('carga-completa')
-      setTimeout(()=>{
-        console.log('borrando pantalla del dom');
-        pantallaCarga.remove();
-      },1000);
-      // pantallaCarga.remove();
-      
+            setTimeout(() => {
+              mensajeFormulario.style.opacity = 0;
+              mensajeFormulario.innerHTML = "";
+            }, 5000);
+          });
+      } else {
+        const listadoMensaje = [];
+        //let errores = Object.values(errors);
 
 
-    },2000)
+        mensajeFormulario.style.opacity = 1;
+        mensajeFormulario.style.color = "red";
+        mensajeFormulario.innerHTML = errors;
 
+        setTimeout(() => {
+          mensajeFormulario.style.opacity = 0;
+          mensajeFormulario.innerHTML = "";
+        }, 5000);
+      }
+    });
 
-
-
-
-    window.addEventListener('scroll', ()=>{
-      console.log(window.pageYOffset)
+    window.addEventListener("scroll", () => {
+      // console.log(window.pageYOffset)
       if (window.pageYOffset >= navbarTop) {
-        desktopNav.classList.add("sticky-navbar")
+        desktopNav.classList.add("sticky-navbar");
+        titularNavbar.classList.add("nav-empresa-activo");
       } else {
         desktopNav.classList.remove("sticky-navbar");
+        titularNavbar.classList.remove("nav-empresa-activo");
       }
-      if(window.pageYOffset>0){
-
-        iconoBajar.style.display = "none";
-
-      }else
-      {
-
-        iconoBajar.style.display = "inline";
-      }
-
-    })
-
-
-
-
-
-
-
+    });
 
     let openedMenu = false;
 
     hamburgerBtn.addEventListener("click", () => {
       if (!openedMenu) {
         hamburgerNavbar.classList.add("open");
-        // hamburgerNavbar.style.animation='open-hamburger 0.5 ease-in';
         openedMenu = true;
       } else {
         hamburgerNavbar.classList.remove("open");
@@ -80,12 +159,13 @@
       }
     });
 
-    cotizaYa.addEventListener("click", serviciosView);
     // Navigation Listeners
+
+    cotizaYa.addEventListener("click", serviciosView);
+
     dkInicio.addEventListener("click", inicioView);
     dkServicios.addEventListener("click", serviciosView);
     dkProyectos.addEventListener("click", proyectosView);
-    // dkInicio.addEventListener("click", contactoView);
     dkContacto.addEventListener("click", contactoView);
 
     mbInicio.addEventListener("click", inicioView);
@@ -95,15 +175,10 @@
 
     function inicioView(e) {
       e.preventDefault();
-      // document.querySelector(".desktop-header").scrollIntoView({
-      //   behavior: "smooth",
-      // });
-      window.scrollTo({top:0, behavior: 'smooth'});
-      
+      window.scrollTo({ top: 0, behavior: "smooth" });
     }
 
     function serviciosView(e) {
-      
       e.preventDefault();
       document.querySelector("#servicios").scrollIntoView({
         behavior: "smooth",
@@ -129,17 +204,5 @@
       });
     }
 
-    // document.getElementById("link-contacto").addEventListener("click", (e) => {
-    //   e.preventDefault();
-    //   document.getElementById("contacto").scrollIntoView({
-    //     behavior: "smooth",
-    //   });
-    // });
-
-    const formConstraints = {
-      name: { presence: { allowEmpty: false } },
-      email: { presence: { allowEmpty: false }, email: true },
-      message: { presence: { allowEmpty: false } },
-    };
   });
 })();
--- somos.js
+++ somos.js
@@ -7,17 +7,19 @@ let openedMenu = false;
 
 const desktopNav = document.querySelector(".desktop-nav");
 const navbarTop = desktopNav.offsetTop;
+const titularNavbar = document.querySelector('.nav-empresa');
 
 
-window.addEventListener('scroll', ()=>{
-
+window.addEventListener("scroll", () => {
+  // console.log(window.pageYOffset)
   if (window.pageYOffset >= navbarTop) {
-    desktopNav.classList.add("sticky-navbar")
+    desktopNav.classList.add("sticky-navbar");
+    titularNavbar.classList.add("nav-empresa-activo")
   } else {
     desktopNav.classList.remove("sticky-navbar");
+    titularNavbar.classList.remove("nav-empresa-activo")
   }
-
-})
+});
 
 hamburgerBtn.addEventListener("click", () => {
   if (!openedMenu) {
